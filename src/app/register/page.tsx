a292a6db9656c2dd9d6eb253b8457c86a8a50429
Pedro Henrique <104235452+pedrohkf@users.noreply.github.com>
2025-03-05 11:15:05 -0300

    feat: adiciona responsividade login e register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,10 +5,8 @@ import styles from "./register.module.css"
 export default function page() {
     return (
         <div className={styles.container}>
-            <div className={styles.ad}>
-                anuncio
-            </div>
-            <div className={styles.content}>
+            <div className={styles.ad}>AD</div>
+            <div className={styles.contentForm}>
                 <RegisterForm />
             </div>
         </div>
