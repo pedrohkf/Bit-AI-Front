897b30892e8f6da563aa54511eb5d19d50334bb1
Pedro Henrique <104235452+pedrohkf@users.noreply.github.com>
2025-03-07 16:31:33 -0300

    fix: adição do google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import HomePage from "./homePage/page";
+import Script from "next/script";
 
 export default function Home() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5RFJH56K92" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5RFJH56K92');
+        `}
+      </Script>
+      <div>
+        <HomePage />
+      </div>
+    </>
   );
 }
